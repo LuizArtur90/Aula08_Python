15aae379bd7a6bf881048459b2db82e74383a7c9
Luiz Artur <luiz.lamoglia@gmail.com>
2024-09-09 18:35:00 -0300

    atualizando comentario

--- pipeline.py
+++ pipeline.py
@@ -3,6 +3,7 @@ from etl import extrair_dados, calcular_kpi_lucro_bruto, carregar_dados
 pasta:str = 'data'
 data_frame = extrair_dados(path=pasta)
 data_frame_calculado = calcular_kpi_lucro_bruto(data_frame)
+#Escolher arquivos de saída .csv, .parquet ou ambos
 formato_de_saida: list = ["parquet","csv"]
 carregar_dados(data_frame_calculado, formato_de_saida)
 
